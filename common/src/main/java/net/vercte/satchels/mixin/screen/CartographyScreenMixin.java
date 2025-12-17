1032929e8b6918bbabf9259454c5327adb74b82b
vercte <lopstedener@gmail.com>
2025-12-16 23:12:32 -0800

    fix cartography table

--- common/src/main/java/net/vercte/satchels/mixin/screen/CartographyScreenMixin.java
+++ common/src/main/java/net/vercte/satchels/mixin/screen/CartographyScreenMixin.java
@@ -3,6 +3,7 @@ package net.vercte.satchels.mixin.screen;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.gui.screens.inventory.CartographyTableScreen;
 import net.minecraft.client.gui.screens.inventory.CraftingScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
@@ -14,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(CraftingScreen.class)
+@Mixin(CartographyTableScreen.class)
 public abstract class CartographyScreenMixin<T extends AbstractContainerMenu> extends AbstractContainerScreen<T> {
     public CartographyScreenMixin(T abstractContainerMenu, Inventory inventory, Component component) { super(abstractContainerMenu, inventory, component); }
 
@@ -23,10 +24,5 @@ public abstract class CartographyScreenMixin<T extends AbstractContainerMenu> ex
     @Inject(method = "renderBg", at = @At("HEAD"))
     public void renderSatchelInventory(GuiGraphics guiGraphics, float f, int i, int j, CallbackInfo ci) {
         satchels$screenWithSatchel.renderSatchelInventory(guiGraphics, this.leftPos, this.topPos, this.imageHeight);
-    }
-
-    @ModifyReturnValue(method = "hasClickedOutside", at = @At("RETURN"))
-    public boolean hasClickedOutside(boolean original, double x, double y, int left, int top, int button) {
-        return ScreenWithSatchel.hasClickedOutside(original, x, y, left, top, this.imageHeight);
     }
 }
