cf04a0e4c921595378b2b9fd7dff1e84eca06e64
vercte <lopstedener@gmail.com>
2025-12-16 22:41:41 -0800

    feature: Satchel makes sound on equip

--- common/src/main/java/net/vercte/satchels/satchel/SatchelEquipmentSlot.java
+++ common/src/main/java/net/vercte/satchels/satchel/SatchelEquipmentSlot.java
@@ -1,5 +1,6 @@
 package net.vercte.satchels.satchel;
 
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
@@ -18,5 +19,15 @@ public class SatchelEquipmentSlot extends Slot {
     @Override
     public boolean mayPickup(Player player) {
         return SatchelData.get(player).getSatchelInventory().isEmpty();
+    }
+
+    @Override
+    public void setByPlayer(ItemStack itemStack) {
+        if(!itemStack.isEmpty()) {
+            Player player = ((SatchelSlotInventory)this.container).getSatchelData().getPlayer();
+            player.playSound(SoundEvents.ARMOR_EQUIP_LEATHER.value(), 1.0f, 0.9f + player.getRandom().nextFloat()/5);
+        }
+
+        this.setByPlayer(itemStack, this.getItem());
     }
 }
--- common/src/main/java/net/vercte/satchels/satchel/SatchelSlotInventory.java
+++ common/src/main/java/net/vercte/satchels/satchel/SatchelSlotInventory.java
@@ -83,4 +83,8 @@ public class SatchelSlotInventory implements Container {
             stack = ItemStack.EMPTY;
         }
     }
+
+    public SatchelData getSatchelData() {
+        return satchelData;
+    }
 }
