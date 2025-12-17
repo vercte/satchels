1c95548fbd72b52cd6e3c225dab30396bde03208
vercte <lopstedener@gmail.com>
2025-12-17 10:27:07 -0800

    fix: you can equip a satchel via right-click

--- common/src/main/java/net/vercte/satchels/ModItems.java
+++ common/src/main/java/net/vercte/satchels/ModItems.java
@@ -3,11 +3,12 @@ package net.vercte.satchels;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Item;
 import net.vercte.satchels.platform.Services;
+import net.vercte.satchels.satchel.SatchelItem;
 
 import java.util.function.Supplier;
 
 public class ModItems {
-    public static Supplier<Item> SATCHEL = register("satchel", () -> new Item(new Item.Properties().stacksTo(1)));
+    public static Supplier<Item> SATCHEL = register("satchel", SatchelItem::new);
 
     private static <T extends Item> Supplier<T> register(String path, Supplier<T> item) {
         return Services.REGISTRY.register(BuiltInRegistries.ITEM, path, item);
